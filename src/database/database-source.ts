df40b8edf39364c8d6f8e78f9a7ecdb94d5e6263
Nayan Ghodadara <nayan.uniqual@gmail.com>
2026-05-24 17:37:21 +0530

    make synchronize false

--- src/database/database-source.ts
+++ src/database/database-source.ts
@@ -17,7 +17,7 @@ export const databaseSourceOption: DataSourceOptions = {
     entities: [
         __dirname + '/../**/*.entity{.ts,.js}'
     ],
-    synchronize: process.env.NODE_ENV !== "production",
+    synchronize: false,
 }
 
 export const dataSource = new DataSource(databaseSourceOption)
