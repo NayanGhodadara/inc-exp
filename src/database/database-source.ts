06545abb0451b75e5d7a58d774fac511cfe1bd49
Nayan Ghodadara <nayan.uniqual@gmail.com>
2026-06-02 10:40:45 +0530

    minor changes

--- src/database/database-source.ts
+++ src/database/database-source.ts
@@ -17,7 +17,7 @@ export const databaseSourceOption: DataSourceOptions = {
     entities: [
         __dirname + '/../**/*.entity{.ts,.js}'
     ],
-    synchronize: true,
+    synchronize: false,
 }
 
 export const dataSource = new DataSource(databaseSourceOption)
