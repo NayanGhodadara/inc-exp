1af79c1936dba8cb727f7d8d162a35a20cc6b07d
Nayan Ghodadara <nayan.uniqual@gmail.com>
2026-06-08 17:33:39 +0530

    minor changes

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
     .setDescription('API documentation for the Inc-Exp application')
     .setVersion('1.0')
     .addBearerAuth()
-    .addServer('http://localhost:2000', 'Local')
     .addServer(process.env.BASE_URL || '', 'Inc-Exp server')
     .build();
 
